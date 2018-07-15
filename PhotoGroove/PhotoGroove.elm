6e4a23ed86af6016153f3d090447ea8ce6c51ed1
Steven Zhang <steven.shutao@gmail.com>
2018-07-15 22:23:11 +0800

    simple initial model added

---
+++ PhotoGroove/PhotoGroove.elm
@@ -0,0 +1,35 @@
+{- Version 2
+
+    initial model added
+ -}
+
+module PhotoGroove exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+initialModel = 
+    [{url = "1.jpeg"}
+    ,{url = "2.jpeg"}
+    ,{url = "3.jpeg"}
+    ]
+
+
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+view model = 
+    div [class "content"]
+        [h1 [] [text "Photo Groove"]
+        , div [id "thumbnails"] (List.map viewThumbnail initialModel)
+        ]
+
+
+viewThumbnail thumbnail = 
+    img [src (urlPrefix ++ thumbnail.url)] []
+
+
+main = 
+    view initialModel
---
+++ PhotoGroove/PhotoGroove01.elm
@@ -0,0 +1,23 @@
+{- Version 1
+
+    No model at all.
+ -}
+
+module PhotoGroove exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+view model = 
+    div [class "content"]
+        [h1 [] [text "Photo Groove"]
+        , div [id "thumbnails"]
+                [img [src "http://elm-in-action.com/1.jpeg"] []
+                , img [src "http://elm-in-action.com/2.jpeg"] []
+                , img [src "http://elm-in-action.com/3.jpeg"] []
+                ]
+        ]
+
+
+main = 
+    view "no model yet"
